8c5e94d856f26f0cc54664601bc01e1f577322e6
Rupmichit Ladep <rupmichitladep@gmail.com>
2023-05-21 12:02:11 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h
 /**
-main -A program that prints inline with puts function
+main - A program that prints inline with puts function
 Return: 0 (success)
 /
-
+	
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
